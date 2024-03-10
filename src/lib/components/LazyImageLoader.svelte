62c9c46fd42242f451f71bdc397d65e1a6e92409
Ryan McCallum <mccallry.dev@gmail.com>
2024-03-10 16:45:19 -0400

    Add lazy loaded images and observer

---
+++ src/lib/components/LazyImageLoader.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let src;
+  export let alt;
+  export let style;
+  
+  import IntersectionObserver from './IntersectionObserver.svelte'
+  import LazyImage from './LazyImage.svelte'
+
+  let nativeLoading = false  
+</script>
+
+<IntersectionObserver once={true} let:intersecting={intersecting}>
+  {#if intersecting || nativeLoading}
+    <LazyImage {alt} {src} {style} on:click />
+  {/if}
+</IntersectionObserver> 
